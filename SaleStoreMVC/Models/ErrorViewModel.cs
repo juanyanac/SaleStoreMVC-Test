663e0da599027b91393ed585137f9fbb9b05169b
Juan Nadar Yanac Barreto <juan.yanac@outlook.com>
2019-07-16 21:13:48 -0300

    created project SaleStore

---
+++ SaleStoreMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SaleStoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
